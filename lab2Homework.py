8351e7ca2edcdeb5345f6fcadd5894b901505fca
cosmin-oros <oroscosmin09@gmail.com>
2023-03-02 20:33:31 +0200

    binary search trees

--- lab2Homework.py
+++ lab2Homework.py
@@ -40,8 +40,70 @@ class BST:
         else:
             return self._search(val, node.right)
 
+    def searchClosest(self, k):
+        return self._searchClosest(self.root, k)
 
-def isPerfectlyBalanced(root):
+    # it recursively searches the left or right subtree depending on whether
+    # k is less than or greater than the value of the current node
+    def _searchClosest(self, node, k):
+        if node is None:
+            return None
+
+        if node.val == k:
+            return node
+
+        elif k < node.val:
+            left_closest = self._searchClosest(node.left, k)
+            if left_closest is None or abs(left_closest.val - k) > abs(node.val - k):
+                return node
+            else:
+                return left_closest
+        else:
+            right_closest = self._searchClosest(node.right, k)
+            if right_closest is None or abs(right_closest.val - k) > abs(node.val - k):
+                return node
+            else:
+                return right_closest
+
+    def checkExistTwoNodesWithSum(self, s):
+        seen = set()
+        return self._checkExistTwoNodesWithSum(self.root, s, seen)
+
+    # it adds the value of the current node to the set of seen values
+    # and recursively searches the left and right subtrees
+    def _checkExistTwoNodesWithSum(self, node, s, seen):
+        if node is None:
+            return False
+        if s - node.val in seen:
+            return True
+        seen.add(node.val)
+        return self._checkExistTwoNodesWithSum(node.left, s, seen) \
+               or self._checkExistTwoNodesWithSum(node.right, s, seen)
+
+
+# IsPerfectlyBalanced - returns true if the BST is perfectly balanced #
+# (A binary tree is said to be perfectly balanced if for every node,
+# the number of nodes in its two subtrees differs by maximum 1).
+# The function must do the check in O(n) time, where n is the number of nodes ! (every node has to be handled just once)
+def IsPerfectlyBalanced(node):
+    if node is None:
+        return True
+    left_subtree_size = count_nodes(node.left)
+    right_subtree_size = count_nodes(node.right)
+
+    # we recursively check if the left and right subtrees are perfectly balanced,
+    # if the sizes of the left and right subtrees differ by at most 1
+    if abs(left_subtree_size - right_subtree_size) <= 1 \
+            and IsPerfectlyBalanced(node.left) \
+            and IsPerfectlyBalanced(node.right):
+        return True
+    return False
+
+
+def count_nodes(node):
+    if node is None:
+        return 0
+    return 1 + count_nodes(node.left) + count_nodes(node.right)
 
 
 #   4
@@ -49,12 +111,15 @@ def isPerfectlyBalanced(root):
 #  5  9
 # /\  /
 # 3 7 11
-tree = BST()
-tree.insert(4)
-tree.insert(5)
-tree.insert(9)
-tree.insert(3)
-tree.insert(7)
-tree.insert(11)
 
-isPerfectlyBalanced(tree)
+bst = BST()
+bst.insert(4)
+bst.insert(5)
+bst.insert(9)
+bst.insert(3)
+bst.insert(7)
+bst.insert(11)
+
+print(IsPerfectlyBalanced(bst.root))
+print(bst.searchClosest(6).val)
+print(bst.checkExistTwoNodesWithSum(14))
