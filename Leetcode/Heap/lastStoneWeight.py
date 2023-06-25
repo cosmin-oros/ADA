2134e0e1781a0bc8f8156a7e9b9f46754c03526b
Cosmin <oroscosmin09@gmail.com>
2023-06-25 18:50:04 +0300

    heap leetcode

---
+++ Leetcode/Heap/lastStoneWeight.py
@@ -0,0 +1,34 @@
+# You are given an array of integers stones where stones[i] is the weight of the ith stone.
+#
+# We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together.
+# Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+#
+# If x == y, both stones are destroyed, and
+# If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+# At the end of the game, there is at most one stone left.
+#
+# Return the weight of the last remaining stone. If there are no stones left, return 0.
+import heapq
+
+
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        # the way to make a max heap make each negative
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+        
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+
+            if second > first:
+                heapq.heappush(stones, first - second)
+
+        # for the case where it's empty
+        stones.append(0)
+        return abs(stones[0])
+
+
+sol = Solution()
+stones = [2, 7, 4, 1, 8, 1]
+print(sol.lastStoneWeight(stones))
