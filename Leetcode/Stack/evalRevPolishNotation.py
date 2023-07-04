7912b162206623d6fc99ba82b2f988b0baad11ec
Cosmin <oroscosmin09@gmail.com>
2023-07-04 12:40:33 +0300

    stack leetcode

---
+++ Leetcode/Stack/evalRevPolishNotation.py
@@ -0,0 +1,36 @@
+# You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+#
+# Evaluate the expression. Return an integer that represents the value of the expression.
+#
+# Note that:
+#
+# The valid operators are '+', '-', '*', and '/'.
+# Each operand may be an integer or another expression.
+# The division between two integers always truncates toward zero.
+# There will not be any division by zero.
+# The input represents a valid arithmetic expression in a reverse polish notation.
+# The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        stack = []
+
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
+
+
+tokens = ["2", "1", "+", "3", "*"]
+sol = Solution()
+print(sol.evalRPN(tokens))
