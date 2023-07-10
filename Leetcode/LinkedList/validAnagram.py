78ed8cab9a2d72c564404d16e8314957ed275de8
Cosmin <oroscosmin09@gmail.com>
2023-07-10 19:56:47 +0300

    linked list leetcode

---
+++ Leetcode/LinkedList/validAnagram.py
@@ -0,0 +1,26 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all
+# the original letters exactly once.
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+
+
+s = "anagram"
+t = "nagaram"
+sol = Solution()
+print(sol.isAnagram(s, t))
