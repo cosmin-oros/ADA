f9ab17a04f6d30afcb6436518b71de5f806bcd51
Cosmin <oroscosmin09@gmail.com>
2023-07-19 21:33:28 +0300

    sliding window leetcode

---
+++ Leetcode/SlidingWindow/longestSubsWithoutRepeatChars.py
@@ -0,0 +1,22 @@
+# Given a string s, find the length of the longest
+# substring
+#  without repeating characters.
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+
+
+s = "abcabcbb"
+sol = Solution()
+print(sol.lengthOfLongestSubstring(s))
