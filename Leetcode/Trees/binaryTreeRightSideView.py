8f4905f038977d1b1271dd74374444265c7575b6
Cosmin <oroscosmin09@gmail.com>
2023-07-22 19:13:17 +0300

    trees leetcode

---
+++ Leetcode/Trees/binaryTreeRightSideView.py
@@ -0,0 +1,41 @@
+# Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes
+# you can see ordered from top to bottom.
+import collections
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def rightSideView(self, root):
+        res = []
+        # init queue with root
+        q = collections.deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return res
+
+
+sol = Solution()
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+print(sol.rightSideView(root))
