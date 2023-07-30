545f87de6495dd8c3c974e5fcad694398a192570
Cosmin <oroscosmin09@gmail.com>
2023-07-30 19:00:04 +0300

    two pointers leetcode

---
+++ Leetcode/TwoPointers/trappingWater.py
@@ -0,0 +1,25 @@
+# Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+class Solution(object):
+    def trap(self, height):
+        if not height:
+            return 0
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
+
+
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+sol = Solution()
+print(sol.trap(height))
