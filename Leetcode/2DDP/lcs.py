d6ac8c2ab79f937502eec18b4c9a2d5f2ba6f9f7
Cosmin <oroscosmin09@gmail.com>
2023-09-06 14:57:47 +0300

    2D dynamic programming leetcode

---
+++ Leetcode/2DDP/lcs.py
@@ -0,0 +1,29 @@
+# Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common
+# subsequence, return 0.
+#
+# A subsequence of a string is a new string generated from the original string with some characters (can be none)
+# deleted without changing the relative order of the remaining characters.
+#
+# For example, "ace" is a subsequence of "abcde".
+# A common subsequence of two strings is a subsequence that is common to both strings.
+
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        # 2D grid init with 0
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                # start at the bottom right
+                if text1[i] == text2[j]:
+                    # 1 + diagonal
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+        return dp[0][0]
+
+
+text1 = "abcde"
+text2 = "ace"
+sol = Solution()
+print(sol.longestCommonSubsequence(text1, text2))
