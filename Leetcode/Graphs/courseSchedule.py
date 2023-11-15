d77d1d7f441c2223c051a71d71da52df04c1af6f
Cosmin <oroscosmin09@gmail.com>
2023-11-15 13:07:11 +0200

    graphs leetcode

---
+++ Leetcode/Graphs/courseSchedule.py
@@ -0,0 +1,41 @@
+# There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array
+# prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take
+# course ai.
+#
+# For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+# Return true if you can finish all courses. Otherwise, return false.
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        # initially map each course to an empty list
+        preMap = { i:[] for i in range(numCourses) }
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visitSet = set()
+
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+            if not preMap[crs]:
+                return True
+
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visitSet.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
+
+
+numCourses = 2
+prerequisites = [[1,0]]
+sol = Solution()
+print(sol.canFinish(numCourses, prerequisites))
