ba66017128744cd340eb804e15521fa27bdfa777
Cosmin <oroscosmin09@gmail.com>
2023-12-04 12:43:51 +0200

    backtrack leetcode

---
+++ Leetcode/Backtracking/letterCombOfPhoneNr.py
@@ -0,0 +1,36 @@
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could
+# represent. Return the answer in any order.
+#
+# A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any
+# letters.
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        res = []
+        digitToChar = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : 'wxyz'
+        }
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+        return res
+
+
+digits = "23"
+sol = Solution()
+print(sol.letterCombinations(digits))
