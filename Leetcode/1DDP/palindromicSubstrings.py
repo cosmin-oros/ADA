78dfb0704b1bdeda307c7efc2e83dd5ae84df761
Cosmin <oroscosmin09@gmail.com>
2024-01-17 10:42:23 +0200

    1D DP leetcode

---
+++ Leetcode/1DDP/palindromicSubstrings.py
@@ -0,0 +1,29 @@
+# Given a string s, return the number of palindromic substrings in it.
+#
+# A string is a palindrome when it reads the same backward as forward.
+#
+# A substring is a contiguous sequence of characters within the string.
+
+class Solution(object):
+    def countSubstrings(self, s):
+        res = 0
+
+        for i in range(len(s)):
+            l = r = i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+
+            l = i
+            r = i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+        return res
+
+
+s = "abc"
+sol = Solution()
+print(sol.countSubstrings(s))
