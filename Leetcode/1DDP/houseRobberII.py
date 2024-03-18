ed59622e2d8237aa29afe7ce5e4618e9fc8aa900
Cosmin <oroscosmin09@gmail.com>
2024-03-18 14:14:40 +0200

    1D DP leetcode

---
+++ Leetcode/1DDP/houseRobberII.py
@@ -0,0 +1,25 @@
+# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money
+# stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last
+# one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if
+# two adjacent houses were broken into on the same night.
+#
+# Given an integer array nums representing the amount of money of each house, return the maximum amount of money you
+# can rob tonight without alerting the police.
+
+class Solution(object):
+    def rob(self, nums):
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            temp = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+
+
+nums = [2, 3, 2]
+sol = Solution()
+print(sol.rob(nums))
