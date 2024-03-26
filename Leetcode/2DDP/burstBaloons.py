6abf4b58da1401ed44bd136bb42ccb91ea942ff4
Cosmin <oroscosmin09@gmail.com>
2024-03-26 12:50:49 +0200

    2D DP leetcode

---
+++ Leetcode/2DDP/burstBaloons.py
@@ -0,0 +1,31 @@
+# You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an
+# array nums. You are asked to burst all the balloons.
+#
+# If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of
+# bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+#
+# Return the maximum coins you can collect by bursting the balloons wisely.
+
+class Solution(object):
+    def maxCoins(self, nums):
+        nums = [1] + nums + [1]
+        dp = {}
+
+        def dfs(l, r):
+            if l > r:
+                return 0
+            if (l, r) in dp:
+                return dp[(l, r)]
+            dp[(l, r)] = 0
+            for i in range(l, r + 1):
+                coins = nums[l-1] * nums[i] * nums[r+1]
+                coins += dfs(l, i-1) + dfs(i+1, r)
+                dp[(l, r)] = max(dp[(l, r)], coins)
+            return dp[(l, r)]
+
+        return dfs(1, len(nums) - 2)
+
+
+nums = [3, 1, 5, 8]
+sol = Solution()
+print(sol.maxCoins(nums))
